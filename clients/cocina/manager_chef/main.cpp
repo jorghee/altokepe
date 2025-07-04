edff5ada1ad03e4113b2a6278e37a778757a3dae
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-07-03 19:12:13 -0500

    Entry to manage chef view

--- clients/cocina/manager_chef/main.cpp
+++ clients/cocina/manager_chef/main.cpp
@@ -1,25 +1,21 @@
-#include "ui/VentanaManager.h"
+#include "network/ClienteManagerApp.h"
 #include <QApplication>
 #include <QFile>
+#include <QDebug>
 
 int main(int argc, char* argv[]) {
   QApplication a(argc, argv);
 
-  // Cargar la hoja de estilos
-  QFile styleFile(":/styles.qss"); // Asumiendo que se añade a los recursos de Qt (qrc)
-  // O para desarrollo rápido:
-  // QFile styleFile("ruta/a/tu/proyecto/clients/cocina/manager_chef/styles.qss");
-  
+  QFile styleFile("styles.qss"); // Busca en el directorio de trabajo
   if (styleFile.open(QFile::ReadOnly)) {
-    QString styleSheet = QLatin1String(styleFile.readAll());
-    a.setStyleSheet(styleSheet);
+    a.setStyleSheet(QLatin1String(styleFile.readAll()));
     styleFile.close();
   } else {
-    qWarning() << "No se pudo cargar la hoja de estilos.";
+    qWarning() << "No se pudo cargar la hoja de estilos 'styles.qss'."; 
   }
 
-  VentanaManager w;
-  w.show();
+  ClienteManagerApp app;
+  app.iniciar();
 
   return a.exec();
 }
