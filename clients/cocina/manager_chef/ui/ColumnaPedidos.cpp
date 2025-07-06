5b913f47a05442dec7a016e18d0e6cad69cda308
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-07-06 12:27:17 -0500

    Add handling methods to choice first order

--- clients/cocina/manager_chef/ui/ColumnaPedidos.cpp
+++ clients/cocina/manager_chef/ui/ColumnaPedidos.cpp
@@ -32,6 +32,16 @@ ColumnaPedidos::ColumnaPedidos(const QString& titulo, QWidget* parent)
   setLayout(mainLayout);
 }
 
+bool ColumnaPedidos::estaVacia() const {
+  return m_layoutContenido->count() == 0;
+}
+
+TarjetaPedido* ColumnaPedidos::getPrimerPedido() const {
+  if (estaVacia()) return nullptr;
+  QLayoutItem* item = m_layoutContenido->itemAt(0);
+  return qobject_cast<TarjetaPedido*>(item->widget());
+}
+
 void ColumnaPedidos::agregarPedido(TarjetaPedido* pedido) {
   m_layoutContenido->addWidget(pedido);
 }
@@ -39,9 +49,7 @@ void ColumnaPedidos::agregarPedido(TarjetaPedido* pedido) {
 void ColumnaPedidos::limpiarPedidos() {
   QLayoutItem* item;
   while ((item = m_layoutContenido->takeAt(0)) != nullptr) {
-    if (item->widget()) {
-      delete item->widget();
-    }
+    if (item->widget()) delete item->widget();
     delete item;
   }
 }
