5f4d14acbea3a9afe5450f4c019c36c6707df223
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-07-06 12:21:52 -0500

    Add granular event management

--- clients/cocina/manager_chef/ui/VentanaManager.h
+++ clients/cocina/manager_chef/ui/VentanaManager.h
@@ -8,6 +8,8 @@
 #include "common/models/PlatoDefinicion.h"
 
 class ColumnaPedidos;
+class TarjetaPedido;
+class QJsonObject;
 
 class VentanaManager : public QWidget {
   Q_OBJECT
@@ -15,10 +17,16 @@ public:
   explicit VentanaManager(QWidget* parent = nullptr);
 
 public slots:
-  void actualizarVistas(const std::vector<PedidoMesa>& pendientes, 
-                      const std::vector<PedidoMesa>& enProgreso, 
-                      const std::vector<PedidoMesa>& terminados,
-                      const std::unordered_map<int, PlatoDefinicion>& menu);
+  void cargarEstadoInicial(
+    const std::vector<PedidoMesa>& pendientes,
+    const std::vector<PedidoMesa>& enProgreso,
+    const std::vector<PedidoMesa>& terminados,
+    const std::unordered_map<int, PlatoDefinicion>& menu
+  );
+  void onPedidoNuevo(const PedidoMesa& pedido, const std::unordered_map<int, PlatoDefinicion>& menu);
+  void onPedidoAMover(long long idPedido, const QString& columnaDestino);
+  void onPedidoAEliminar(long long idPedido);
+  void onActualizarEstadoPlato(long long idPedido, long long idInstancia, const QString& nuevoEstado);
 
 signals:
   void prepararPedidoSolicitado(long long idPedido);
@@ -28,10 +36,14 @@ signals:
 
 private:
   void setupUI();
+  TarjetaPedido* crearTarjeta(const PedidoMesa& pedido, const std::unordered_map<int, PlatoDefinicion>& menu);
+  void actualizarBotonesColumnaPendiente();
   
   ColumnaPedidos* m_columnaPendiente;
   ColumnaPedidos* m_columnaEnProgreso;
   ColumnaPedidos* m_columnaTerminado;
+
+  std::unordered_map<long long, TarjetaPedido*> m_mapaTarjetas;
 };
 
 #endif
